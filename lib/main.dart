03177da956573d546e8968bc2d782af35a885743
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2023-06-01 22:25:59 +0700

    Main Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: MaterialApp(
-        home: Center(
-          child: Text("Test"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerRight,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.deepPurple,
+                Colors.deepPurpleAccent
+              ]
+            )
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset("assets/images/quiz-logo.png", width: 200.0,),
+                const SizedBox(height: 40.0),
+                const Text("Learn Flutter the fun way!", style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white
+                )),
+                const SizedBox(height: 40.0),
+                OutlinedButton(
+                    onPressed: (){},
+                    child: const Text("Start Quiz",
+                    style: TextStyle(
+                      color: Colors.white
+                    ),)
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
