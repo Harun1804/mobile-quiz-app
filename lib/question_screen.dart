85bb75b798a34643c7c55141039c7ce08091ef74
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2023-06-09 10:28:47 +0700

    Use the list data component

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,18 +12,21 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text("Judul",
-          style: TextStyle(
+          Text(currentQuestion.text,
+          style: const TextStyle(
             color: Colors.white
           ),),
           const SizedBox(height: 30.0,),
-          AnswerButton(answerText: "Answer 1", onTap: () {}),
-          AnswerButton(answerText: "Answer 2", onTap: () {}),
-          AnswerButton(answerText: "Answer 3", onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
