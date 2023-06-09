3767675a01e7c409aeea87eac9f1bd4ec900dbe9
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2023-06-09 09:15:35 +0700

    set to ternary operation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,17 +10,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  String activeScreen = 'start-screen';
 
   void switchScreen () {
     setState(() {
-      activeScreen = const QuestionScreen();
+      activeScreen = 'question-screen';
     });
   }
 
@@ -40,7 +34,7 @@ class _QuizState extends State<Quiz> {
                     ]
                 )
             ),
-            child: activeScreen
+            child: activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionScreen()
         ),
       ),
     );
