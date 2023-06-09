61207c7080ed4c97e320e39574c6717dcb092e52
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2023-06-09 09:52:18 +0700

    Working with model and data

---
+++ lib/data/questions.dart
@@ -0,0 +1,28 @@
+import 'package:quiz/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('Siapa Penemu Bola Lampu', [
+    'Thomas Alpha Edison',
+    'John Wick',
+    'Nicola Tesla',
+    'Jeane d`arc'
+  ]),
+  QuizQuestion('Apa Lambang Negara Indonesia', [
+    'Garuda',
+    'Macam',
+    'Naga',
+    'Elang'
+  ]),
+  QuizQuestion('Bentuk Awal Lamborghini', [
+    'Traktor',
+    'Mobil Sport',
+    'Motor',
+    'Handphone'
+  ]),
+  QuizQuestion('Bernafas tapi tak bernyawa', [
+    'Waktu Fajar',
+    'Malam',
+    'Pegunungan',
+    'Embun Pagi'
+  ]),
+];
