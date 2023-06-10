105ece5644f7fd3833d03797043e5fc2e85f7ba5
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2023-06-11 05:49:29 +0700

    Change Laylouting

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -14,19 +14,27 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(BuildContext context) {
     final currentQuestion = questions[0];
 
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(currentQuestion.text,
-          style: const TextStyle(
-            color: Colors.white
-          ),),
-          const SizedBox(height: 30.0,),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: (){});
-          })
-        ],
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white
+              ),
+            ),
+            const SizedBox(height: 30.0,),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: (){});
+            })
+          ],
+        ),
       ),
     );
   }
